d43a62c0f66c5b9c48bbf7b2335916506752a47c
sashacozub <81567588+sashacozub@users.noreply.github.com>
2021-07-24 14:40:36 +0200

    Finish main code for the slider

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,52 @@
+// Get DOM elements
 const buttonLeft = document.querySelector('.btn-left');
 const buttonRight = document.querySelector('.btn-right');
 const slider = document.querySelector('.images-slider');
+const sliderImages = document.querySelectorAll('.images-slider img');
 
-const sliderSize = slider.clientWidth;
-let counter = 0;
+// Set the width of the slider
+let sliderSize = slider.clientWidth;
 
-buttonRight.addEventListener('click', () => {
-    counter++;
-    slider.style.transform = `translateX(${-sliderSize * counter}px)`
+// Set counter to first image and not the clone of the last image
+let counter = 1;
+slider.style.transform = `translateX(${-sliderSize * counter}px)`;
+
+// Slide image to previous image
+buttonLeft.addEventListener('click', () => {
+    // This ensures the slider won't overshoot if pressed to quick
+    if (counter < 1) { return };
+    slider.style.transition = 'all 400ms ease-out';
+    counter--;
+    slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 })
 
-buttonLeft.addEventListener('click', () => {
-    counter--;
-    slider.style.transform = `translateX(${-sliderSize * counter}px)`
+// Slide image to next image
+buttonRight.addEventListener('click', () => {
+    // This ensures the slider won't overshoot if pressed to quick
+    if (counter > sliderImages.length - 2) { return };
+    slider.style.transition = 'all 400ms ease-out';
+    counter++;
+    slider.style.transform = `translateX(${-sliderSize * counter}px)`;
+})
+
+// Jump from image clone to the actual image
+slider.addEventListener('transitionend', () => {
+    if (sliderImages[counter].id === 'last-clone') {
+        // No transition will hide the jump
+        slider.style.transition = 'none';
+        counter = sliderImages.length - 2;
+        slider.style.transform = `translateX(${-sliderSize * counter}px)`;
+    }
+
+    if (sliderImages[counter].id === 'first-clone') {
+        slider.style.transition = 'none';
+        counter = 1;
+        slider.style.transform = `translateX(${-sliderSize * counter}px)`;
+    }
+})
+
+// If window size is changes, also change the slider size and position the current image correctly
+window.addEventListener('resize', () => {
+    sliderSize = slider.clientWidth;
+    slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 })
