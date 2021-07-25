67af1c72bfc95675063428081d8656394da12afa
sashacozub <81567588+sashacozub@users.noreply.github.com>
2021-07-26 01:26:06 +0200

    Make design responsive

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const buttonRight = document.querySelector('#btn-right');
 const slider = document.querySelector('.images-slider');
 const sliderImages = document.querySelectorAll('.images-slider img');
 const dots = document.querySelectorAll('div[data-dot]');
+const autoplay = document.querySelector('.autoplay');
+const autoplayToggle = document.querySelector('.toggle');
 
 // Create element for active dot
 const activeDot = document.createElement('div');
@@ -19,20 +21,20 @@ slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 dots[counter - 1].appendChild(activeDot);
 
 
-// Slide image to previous image
+// Slide to previous image
 buttonLeft.addEventListener('click', () => {
     // This ensures the slider won't overshoot if pressed to quick
     if (counter < 1) { return };
-    slider.style.transition = 'all 400ms ease-out';
+    slider.style.transition = 'all 400ms ease';
     counter--;
     slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 })
 
-// Slide image to next image
+// Slide to next image
 buttonRight.addEventListener('click', () => {
     // This ensures the slider won't overshoot if pressed to quick
     if (counter > sliderImages.length - 2) { return };
-    slider.style.transition = 'all 400ms ease-out';
+    slider.style.transition = 'all 400ms ease';
     counter++;
     slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 })
@@ -58,11 +60,19 @@ slider.addEventListener('transitionend', () => {
 })
 
 
+// Change to appropriate image when one of the dots is clicked
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        slider.style.transition = 'all 400ms ease';
+        counter = dot.dataset.dot;
+        slider.style.transform = `translateX(${-sliderSize * counter}px)`;
+        dots[counter - 1].appendChild(activeDot);
+    })
+})
+
+
 // If window size is changes, also change the slider size and position the current image correctly
 window.addEventListener('resize', () => {
     sliderSize = slider.clientWidth;
     slider.style.transform = `translateX(${-sliderSize * counter}px)`;
 })
-
-
-
